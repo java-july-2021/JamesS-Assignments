c3eb603651ea1c8ef1e371f97e6e0cd126eaa3e2
James Schooley <jamesschooley24@gmail.com>
2021-07-08 10:59:09 -0600

    My First Java Program

---
+++ 01-java-fundamentals/MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        
+        String myName = "Hello, my name is James";
+        System.out.println(myName);
+        String myAge = "I am 33 years old";
+        System.out.println(myAge);
+        String myLocation = "I live in Boise, Idaho";
+        System.out.println(myLocation);
+
+    }
+}
