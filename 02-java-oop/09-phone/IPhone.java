5d150116f94a808c5d0ae49b1d142882914b35a0
James Schooley <jamesschooley24@gmail.com>
2021-07-19 01:54:46 -0600

    phone assignment

---
+++ 02-java-oop/09-phone/IPhone.java
@@ -0,0 +1,21 @@
+public class IPhone extends Phone implements Ringable {
+    public IPhone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+    @Override
+    public String ring() {
+        return this.getRingTone();
+    }
+    @Override
+    public String unlock() {
+        return "unlocked with fingerprint";
+    }
+    @Override
+    public void displayInfo() {
+        System.out.println("Version Number: "+ this.getVersionNumber());
+        System.out.println("Battery Percentage: ", this.getBatteryPercentage());
+        System.out.println("Carrier: ", + this.getCarrier());
+        System.out.println("RingTone: ", + this.getRingTone());
+    }
+}
+
---
+++ 02-java-oop/09-phone/PhoneTester.java
@@ -0,0 +1,6 @@
+public class PhoneTester{
+    public static void main(String[] args) {
+        Galaxy s9 = new Galaxy ("S9",99,"Verizon","Ring Ring Ring");
+        IPhone iPhoneTen = new IPhone("X", 100, "AT&T", "Zing");
+    }
+}
